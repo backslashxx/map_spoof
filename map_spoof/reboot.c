03d15d0011134ec0b200e1f0ef2fd614c8f0bb09
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-10-21 09:31:53 +0800

    map_spoof, userspace: reuse user pointer for command confirmation
    also update a lot of things.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- map_spoof/reboot.c
+++ map_spoof/reboot.c
@@ -9,12 +9,15 @@
 #include <linux/smp.h>
 #include <linux/printk.h>
 
+#ifdef MODULE
+#error "only build me as built-in (CONFIG_MAP_SPOOF=y)"
+#endif
+
 #define DEF_MAGIC 0x11111111
-#define PRINT_ARG 1
-#define CUSTOM_CMD1 11001 // add to list
-#define CUSTOM_CMD2 11002 // destroy list
-#define CUSTOM_CMD3 11003 // skip rwxp enable
-#define CUSTOM_CMD4 11004 // skip rwxp disable
+#define CMD_ADD_TO_LIST 11001
+#define CMD_DESTROY_LIST 11002
+#define CMD_ENABLE_SKIP_RWXP 11003
+#define CMD_DISABLE_SKIP_RWXP 11004
 
 struct string_entry {
     char *string;
@@ -25,10 +28,7 @@ LIST_HEAD(string_list);
 atomic_t skip_rwxp = ATOMIC_INIT(0);
 EXPORT_SYMBOL(skip_rwxp); 
 
-static void __exit reboot_lkm_exit(void) 
-{
-	pr_info("LKM: unload\n");
-}
+static void __exit mapspoof_exit(void) {}
 
 // SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd,
 //		void __user *, arg)
@@ -39,28 +39,20 @@ static void __exit reboot_lkm_exit(void)
 // cmd, unusable as ptr on 64-bit :(, maybe can be used as delimiter of some sort
 // arg, data input, already user ptr so good
 
-
-int lkm_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user *arg)
+int lkm_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg)
 {
-	char buf[256] = {0};
+	int ok = DEF_MAGIC;
 
-	if (magic1 != DEF_MAGIC)
+	if (magic1 != ok)
 		return 0;
 
-	pr_info("LKM: intercepted call! magic: 0x%d id: 0x%d\n", magic1, magic2);
+	pr_info("map_spoof: intercepted call! magic: 0x%x id: %d\n", magic1, magic2);
 
-	if (magic2 == PRINT_ARG) {
-		if (copy_from_user(buf, arg, 256))
-			return 0;
+	if (magic2 == CMD_ADD_TO_LIST) {
+		char buf[256] = {0};
 
-		buf[255] = '\0';
-		pr_info("LKM: print %s\n", buf);
-	}
-
-	if (magic2 == CUSTOM_CMD1) {
-		memzero_explicit(buf, 256);
 		struct string_entry *new_entry, *entry;
-		if (copy_from_user(buf, (const char __user *)arg, sizeof(buf) - 1))
+		if (copy_from_user(buf, (const char __user *)*arg, sizeof(buf) - 1))
 			return 0;
 
 		buf[255] = '\0';
@@ -77,24 +69,24 @@ int lkm_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user 
 		
 		list_for_each_entry(entry, &string_list, list) {
 			if (!strcmp(entry->string, buf)) {
-				pr_info("LKM: %s is already here!\n", buf);
+				pr_info("map_spoof: %s is already here!\n", buf);
 				kfree(new_entry->string);
 				kfree(new_entry);
 				return 0;
 			}
 		}
 		
-		pr_info("LKM: entry %s added!\n", buf);
+		pr_info("map_spoof: entry %s added!\n", buf);
 		list_add(&new_entry->list, &string_list);
 		smp_mb();
 
 	}
 	
-	if (magic2 == CUSTOM_CMD2) {
+	if (magic2 == CMD_DESTROY_LIST) {
 		struct string_entry *entry, *tmp;
 
 		list_for_each_entry_safe(entry, tmp, &string_list, list) {
-        		pr_info("LKM: entry %s removed!\n", entry->string);
+        		pr_info("map_spoof: entry %s removed!\n", entry->string);
         		list_del(&entry->list);
         		kfree(entry->string);
         		kfree(entry);
@@ -102,33 +94,36 @@ int lkm_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user 
         	smp_mb();
 	}
 
-	if (magic2 == CUSTOM_CMD3) {
+	if (magic2 == CMD_ENABLE_SKIP_RWXP) {
 		atomic_set(&skip_rwxp, 1);
-		pr_info("LKM: skip_rwxp: 1\n");
+		pr_info("map_spoof: skip_rwxp: 1\n");
 
 	}
 
-	if (magic2 == CUSTOM_CMD4) {
+	if (magic2 == CMD_DISABLE_SKIP_RWXP) {
 		atomic_set(&skip_rwxp, 0);
-		pr_info("LKM: skip_rwxp: 0\n");
+		pr_info("map_spoof: skip_rwxp: 0\n");
 	}
 
+	// reply to the same pointer
+	if (copy_to_user((void __user *)*arg, &ok, sizeof(ok)))
+		return 0;
+
 	return 0;
 }
 
-static int __init reboot_lkm_init(void) 
+static int __init mapspoof_init(void) 
 {
-	int magic = DEF_MAGIC;
-	pr_info("LKM: init with magic: 0x%d\n", magic);
+	pr_info("map_spoof: init with magic: 0x%x\n", (int)DEF_MAGIC);
 	return 0;
 }
 
-module_init(reboot_lkm_init);
-module_exit(reboot_lkm_exit);
+module_init(mapspoof_init);
+module_exit(mapspoof_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("xx");
-MODULE_DESCRIPTION("reboot lkm chat");
+MODULE_DESCRIPTION("map spoof handler");
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
 MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
--- userspace.c
+++ userspace.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/syscall.h>
 #include <unistd.h>
+#include <stdint.h>
 
 int main(int argc, char *argv[]) {
 	if (argc != 4) {
@@ -15,6 +17,10 @@ int main(int argc, char *argv[]) {
 
 	printf("SYS_reboot(%lu, %lu, 0, %p)\n", magic1, magic2, arg);
 	syscall(SYS_reboot, magic1, magic2, 0, arg);
+
+	// reuse the pointer
+	// printf("reply: 0x%x\n", *(unsigned int *)arg);
+	printf("reply: %s value: 0x%x\n", (magic1 == *(unsigned int *)arg) ? "ok" : "fail", *(unsigned int *)arg);
 	
 	return 0;
 
